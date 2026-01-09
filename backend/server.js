b6f205442d35a07721c04acf13e6a7b8de3b3a1a
Areeba Mohsin <mohsinareeba569@gmail.com>
2026-01-09 10:45:23 +0530

    Setup clean frontend-backend structure with gitignore

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shopping_portal")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
